070cbe162e3ef7f153b89e84a7a830f3661a3783
Joel Triemstra <>
2018-11-28 22:18:50 -0500

    loggers working on webapp

---
+++ discovery-demo/src/main/java/com/example/jtriemstra/spring/discoverydemo/DiscoveryDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.jtriemstra.spring.discoverydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryDemoApplication.class, args);
+	}
+}
